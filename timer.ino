d9ce473fefb0d36ae2f82d9a28852d4e2ae029ef
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-28 16:58:02 +0900

    センサーを使う

--- timer.ino
+++ timer.ino
@@ -24,8 +24,8 @@ const int echo_port = 14;
 const int LED_A_port = 2;
 const int LED_digit_port = 9;
 unsigned long int start_time_ms;
-unsigned long int time_diff;
 bool is_timer_enable = false;
+
 void setup() {
 	Serial.begin(9600);
 	for(int i=0;i<7;++i){
@@ -47,22 +47,25 @@ void loop() {
 		digitalWrite(trig_port, HIGH);
 		delayMicroseconds(11);
 		digitalWrite(trig_port, LOW);
-		//Serial.println(time_diff);
 		int sensor_value;
-		////////////////////////////////////////////////////////////////////////////////////////////////////sensor_value = pulseIn(echo_port, HIGH);
+		sensor_value = pulseIn(echo_port, HIGH);
+		Serial.println(sensor_value);
 		float dist = sensor_value * 0.017;
 		//dist = 1000.0; //DEBUG用
 		if (dist > 4.0) {
 			is_timer_enable = true;
 		}
-		time_diff = millis() - start_time_ms;
+		show_value_to_7seg(millis() - start_time_ms);
 	}
+}
+
+void show_value_to_7seg(unsigned long value){
 	unsigned long digit_selecter=1000;//TODO:いい変数名考えて
 	for(int digit=0;digit<4;++digit){
 		for(int segment=0;segment<4;++segment){
 			digitalWrite(LED_digit_port+segment, LED_digit[digit][segment]);
 		}
-		seg((time_diff / digit_selecter) % 10);
+		seg((value / digit_selecter) % 10);
 		digit_selecter*=10;
 	}
 }
@@ -71,6 +74,6 @@ void seg(int num) {
 	for(int seg_i=0;seg_i<7;++seg_i){
 		digitalWrite(LED_A_port+seg_i,LED7segment[num][seg_i]);
 	}
-	delay(5);
+	delay(3);
 }
 
